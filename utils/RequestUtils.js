dece5aacf7014ce9ced6b3d43e1dfc5a4be1ca9d
“Rajat <rajatvermaa95@gmail.com>
2024-02-04 00:17:05 +0530

    #11: Optimizations for Reporting

--- utils/RequestUtils.js
+++ utils/RequestUtils.js
@@ -1,24 +1,51 @@
 class RequestUtils {
-   
-    async get(request, endpoint) {
-        return await request.get(endpoint);
+
+    logDetails(prefix, details) {
+      console.log('------------------------------------------------------------');
+      console.log(`${prefix} Details:`, details);
     }
-
-    async post(request, endpoint, requestBody) {
-        return await request.post(endpoint, {
-            data: requestBody
-        });
+  
+    logRequestDetails(method, endpoint, requestBody) {
+      this.logDetails('Request', {
+        method,
+        endpoint,
+        requestBody: (method === 'GET' || method === 'DELETE') ? 'N/A' : JSON.stringify(requestBody),
+      });
     }
-
-    async put(request, endpoint, requestBody) {
-        return await request.put(endpoint, {
-            data: requestBody
-        });
+  
+    logResponseDetails(response) {
+      this.logDetails('Response', {
+        status: response.status(),
+        statusText: response.statusText(),
+        url: response.url(),
+        // headers: response.headers()
+      });
+    }
+  
+    async makeRequest(request, method, endpoint, requestBody) {
+      this.logRequestDetails(method, endpoint, requestBody);
+      const response = await request[method.toLowerCase()](endpoint, { data: requestBody });
+      this.logResponseDetails(response);
+      return response;
+    }
+  
+    get(request, endpoint) {
+      return this.makeRequest(request, 'GET', endpoint);
+    }
+  
+    post(request, endpoint, requestBody) {
+      return this.makeRequest(request, 'POST', endpoint, requestBody);
+    }
+  
+    put(request, endpoint, requestBody) {
+      return this.makeRequest(request, 'PUT', endpoint, requestBody);
+    }
+  
+    delete(request, endpoint) {
+      return this.makeRequest(request, 'DELETE', endpoint);
+    }
+    
   }
-
-    async delete(request, endpoint) {
-        return await request.delete(endpoint);
-  }
-}
-
-export default new RequestUtils;
+  
+  export default new RequestUtils();
+  
--- utils/ResponseUtils.js
+++ utils/ResponseUtils.js
@@ -3,7 +3,10 @@ class ResponseUtils {
     async parseAndLog(response) {
       try {
         const responseBody = JSON.parse(await response.text());
+        console.log('------------------------------------------------------------');
+        console.log('Parsed Response Body:');
         console.log(responseBody);
+        console.log('------------------------------------------------------------');
         return responseBody;
       } catch (error) {
         console.error('Error parsing response:', error.message);
